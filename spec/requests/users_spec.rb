a8d3c93188fe002d9ca4c52ed05ff83e9e4895ab
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-01-05 18:51:31 -0300

    test requests get

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+    it "returns success status" do
       get users_path
       expect(response).to have_http_status(200)
+    end
+  end
+  it 'the users title is present' do
+    users = create_list(:user, 3)
+    get users_path
+    users.each do |user|
+      expect(response.body).to include(user.title)
     end
   end
 end
